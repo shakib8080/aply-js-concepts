e452fb494514241ae8e2dada825656512ce5f6b4
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-07-01 16:35:26 +0600

    aply-js-done

---
+++ home-work.js
@@ -0,0 +1,46 @@
+// // function getfarenheit(celsius){
+// //     let farenhit= (celsius * 9.0 / 5.0) + 32.0; 
+// //     return farenhit;
+
+// // }
+// // let today =20;
+// // result = getfarenheit (today);
+// // console.log(result);
+
+// function getcelsius(fahrenheit){
+//     let celsius= ( fahrenheit - 32) * 5/9; 
+//     return celsius;
+
+// }
+// let today =20;
+// let tommorow = 40;
+// result = getcelsius (today);
+// console.log(result);
+// console.log(getcelsius(tommorow)) ;
+
+// grad calculaletion 
+
+let bangla ,english ,arabic,math
+bangla = 100
+english = 85
+arabic = 84
+math = 77
+
+totall = bangla + english+ arabic +math 
+totall = totall / 4
+// console.log(totall);
+if(totall >=80 ){
+    console.log('grade A+');
+}
+else if (totall >= 70){
+    console.log('grade A')
+}
+else if (totall >= 60){
+    console.log('grade A-');
+}
+else if (totall >=60){
+    console.log('grade c')
+}
+else{
+    console.log('low grade');
+}
---
+++ practise.js
@@ -0,0 +1,146 @@
+// push and pop
+
+// let name = ['shakib','rakib', 'ashik'];
+// name.push('zahid');
+// console.log(name);
+// name.pop();
+// console.log(name);
+
+// change Array Element
+
+// let name = ['shakib','rakib', 'ashik'];
+// name[2] ='nahid';
+// console.log(name);
+
+// check index
+
+// let names = ['shakib','rakib', 'ashik'];
+// let foundindex = names [2];
+// console.log(foundindex);
+
+
+// let names = ['shakib','rakib', 'ashik'];
+// console.log(names.indexOf('rakib'));
+
+// ichtofeet 
+function ichtofeet (inches){
+    let feet = inches /12 ;
+    return feet;
+
+    }
+    let myinches = 120 ;
+    let sisterinches = 240;
+    getfeet = ichtofeet (myinches);
+    console.log(getfeet);
+    getfeet = ichtofeet (sisterinches);
+    console.log(getfeet);
+
+// odd and even 
+
+// function isodd (number){
+//     if(number % 2 == 0 ){
+//         return true , 'is odd number';
+//     }
+//     else{
+//         return false,'is is even';
+//     }
+// }
+// let firstnumber =48493;
+// let secoundnumber = 9483;
+// result = isodd(firstnumber);
+// console.log(result);
+
+// factorial while loop
+
+// let factorial = 1;
+// let i = 1;
+// while(i <= 9){
+//     i++;
+//     factorial = factorial *i ;
+  
+// }
+// console.log(factorial);
+
+//  factorial for  loop
+
+// let factorial = 1;
+// for(let i= 1 ; i <=10 ; i++){
+//     factorial = factorial*i;
+// }
+// console.log(factorial);
+
+// function with factorial for loop
+
+// function getfactorial (number){
+//     let factorial = 1;
+//     for(let i = 1; i <=number; i++){
+//         factorial = factorial*i;
+//     }
+//     return factorial;
+// }
+//  let myfactorial = getfactorial(7);
+// console.log(myfactorial);
+
+// function with factorial for loop
+
+// function getfactorial (number){
+//     let factorial = 1;
+//     let i = 1;
+//     while(i <=number){
+//         i++;
+//         factorial = factorial *i ;
+//     }
+//     return factorial;
+// }
+// myfactorial = getfactorial (5);
+// console.log(myfactorial);
+
+// revers function with factorial for loop
+// function getfactorial (number ){
+//     let factorial = 1; 
+//     for(i = number ; i >=1; i--){
+//      factorial = factorial*i ;
+
+//     }
+//     return factorial;
+// }
+// mynuber = getfactorial (9);
+// console.log(mynuber);
+
+// if else diye odd and even \\
+
+// function iseven(number ){
+//     if(number % 2 != 0){
+//         return true,'is even';
+//     }
+//     else{
+//         return false,'is odd' ;
+//     }
+// }
+// mynumber = iseven (48938);
+// console.log(mynumber);
+
+// leapyear with function
+
+// function isleapyear (year){
+//     if(year % 4 == 0 ){
+//     return true ,'is leapyear';
+//     }
+//     else{
+//         return false,'not leapyear';
+//     }
+// }
+
+// let ammaasked = 2921;
+// let abbaasked = 2024;
+// getleapyear = isleapyear (abbaasked);
+// console.log(getleapyear);
+// getleapyear = isleapyear (ammaasked);
+// console.log(getleapyear);
+
+
+
+
+
+
+
