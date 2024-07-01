7f57121221691af85c209f0df6badd10718fed1d
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-07-01 17:11:27 +0600

    hmffd

--- home-work.js
+++ home-work.js
@@ -20,27 +20,36 @@
 
 // grad calculaletion 
 
-let bangla ,english ,arabic,math
-bangla = 100
-english = 85
-arabic = 84
-math = 77
+// let bangla ,english ,arabic,math
+// bangla = 100
+// english = 85
+// arabic = 84
+// math = 77
 
-totall = bangla + english+ arabic +math 
-totall = totall / 4
-// console.log(totall);
-if(totall >=80 ){
-    console.log('grade A+');
-}
-else if (totall >= 70){
-    console.log('grade A')
-}
-else if (totall >= 60){
-    console.log('grade A-');
-}
-else if (totall >=60){
-    console.log('grade c')
-}
-else{
-    console.log('low grade');
-}
+// totall = bangla + english+ arabic +math 
+// totall = totall / 4
+// // console.log(totall);
+// if(totall >=80 ){
+//     console.log('grade A+');
+// }
+// else if (totall >= 70){
+//     console.log('grade A')
+// }
+// else if (totall >= 60){
+//     console.log('grade A-');
+// }
+// else if (totall >=60){
+//     console.log('grade c')
+// }
+// else{
+//     console.log('low grade');
+// }
+
+
+// simple interested 
+
+// let primary = 20;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+// let rate =5;
+// let time = 2;
+// let si = primary *time * rate /100;
+// console.log(si ,'simple interested');
